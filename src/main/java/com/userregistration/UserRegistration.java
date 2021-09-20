522727cf7bfaa9795e6ba61184c7d4d1f4a26952
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-21 00:16:15 +0530

    UC2 has been created where last name is tested and validated

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -5,12 +5,18 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
 	public static void main(String[] args) {
-		firstName("Zero");
+		System.out.println(firstName("Max"));
+		System.out.println(lastName("Payne"));
 	}
 	
 	public static boolean firstName(String firstName) {
 		String patternFirstName = "^[A-Z]{1}[a-zA-Z]{2,}";
 		return Pattern.matches(patternFirstName, firstName);
+	}
+	
+	public static boolean lastName(String lastName) {
+		String patternLastName = "^[A-Z]{1}[a-zA-Z]{2,}";
+		return Pattern.matches(patternLastName, lastName);
 	}
 
 }
